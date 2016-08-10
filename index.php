364381df657c294700d7d87b416aafcbebde0c4d
Dale Yarborough <daleyarborough@gmail.com>
2016-08-10 01:18:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -39,5 +39,24 @@ for ($i = 0; $i <= 5; $i++) {
 
 ?>
 
+<?php
+	echo "<br />";
+	function add_subt($val1, $val2) {
+		$add = $val1 + $val2;
+		$subt = $val1 - $val2;
+		return array($add, $subt);
+	}
+	
+	$result_array = add_subt(10, 5);
+	echo "Add: " . $result_array[0] . "<br />";
+	echo "Subtract: " . $result_array[1] . "<br />";
+	
+	list($add_result, $subt_result) = add_subt(20, 7);
+	echo "Add: " . $add_result . "<br />";
+	echo "Subtract " . $subt_result . "<br />";
+	
+	
+?>
+
 </body>
 </html>
